e69bd6faf5bbcee572acf72a0ac92502d1068deb
j-i.j=i <sunmlee126@gmail.com>
2021-04-29 23:39:13 +0900

    210429

--- JS/BOJ_ 1712.js
+++ JS/BOJ_ 1712.js
@@ -1,24 +1,9 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-//input = input.split(' ');
-function Solution(input){
-    if(Number(input[1])>=Number(input[2]))
-    {
-        return -1;
-    }else{
-        let sellamount = Number(input[0]);
-        let makeitem = Number(input[1]);
-        let profit = Number(input[2]);
-        let i = 1;
-        let x = 0, y = 0;
-        while(true){
-            x = makeitem * i;
-            y = profit * i;
-            if(y>(sellamount+x)){
-                return i;
-            }
-            i++;
-        }
-    }
-}
 
-console.log(Solution(input));
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+    
+let margin = c-b ;
+let count = Math.floor(a/margin)+1;
+console.log(margin<=0 ? -1: count); 
