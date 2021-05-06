c6212b322aa0710e419ae42ad0988682922a4af7
j-i.j=i <sunmlee126@gmail.com>
2021-05-06 23:42:50 +0900

    210506

--- JS/BOJ_ 3009.js
+++ JS/BOJ_ 3009.js
@@ -1,1 +1,35 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+ 
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+ 
+// For local test
+const input = ['30 20', '10 10', '10 20'];
+const strToInt = input.map(coords =>
+coords.split(' ').map(num => parseInt(num))
+);
+ 
+let x = 0;
+let y = 0;
+if (strToInt[0][0] !== strToInt[1][0]) {
+if (strToInt[0][0] !== strToInt[2][0]) {
+x = strToInt[0][0];
+} else {
+x = strToInt[1][0];
+}
+} else {
+x = strToInt[2][0];
+}
+ 
+if (strToInt[0][1] !== strToInt[1][1]) {
+if (strToInt[0][1] !== strToInt[2][1]) {
+y = strToInt[0][1];
+} else {
+y = strToInt[1][1];
+}
+} else {
+y = strToInt[2][1];
+}
+ 
+console.log(`${x} ${y}`);
