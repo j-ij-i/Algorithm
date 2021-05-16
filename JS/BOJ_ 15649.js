779062828a5172104667b2b5b327ec8596359065
j-i.j=i <sunmlee126@gmail.com>
2021-05-16 22:53:26 +0900

    210516

---
+++ JS/BOJ_ 15649.js
@@ -0,0 +1,27 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const input = [4, 2];
+const N = input.shift();
+const M = input.shift();
+
+const visited = new Array(N);
+let output = [];
+let result = '';
+
+function dfs(cnt) {
+  if (cnt === M) {
+    result += `${output.join(' ')}\n`;
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (visited[i] === true) continue;
+    visited[i] = true;
+    output.push(i + 1);
+    dfs(cnt + 1);
+    output.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+console.log(result.trim());
