3cfede78334342d8b447cb5f738d9e2e3a6466b6
j-ij-i <sunmlee126@gmail.com>
2021-09-06 23:53:42 +0900

    210906

---
+++ Java/BOJ_2648.java
@@ -0,0 +1,76 @@
+package algo_0906;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_2468 {
+	static int arr[][];
+	static int dx[] = {0,0,1,-1};
+	static int dy[] = {1,-1,0,0};
+	static int max, N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st ;
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for(int x =0 ; x < N ; x++) {
+			st = new StringTokenizer(br.readLine());
+			for(int y = 0; y < N ; y++) {
+				arr[x][y] = Integer.parseInt(st.nextToken());
+			}
+		}
+		max = 1;
+		BFS(1);
+		
+		System.out.println(max);
+	}
+	
+	private static void BFS(int i) {
+		Queue<int[]> q = new LinkedList<>();
+		while(true) { 
+			//i층에 빗물이 쌓임
+			int cnt = 0;
+			boolean rainarea[][] = new boolean[N][N];
+			for(int x =0 ; x < N ; x++) {
+				for(int y = 0; y < N ; y++) {
+					if(arr[x][y] <= i) {
+						rainarea[x][y] = true;
+						cnt ++;
+					}
+				}
+			}
+			if( cnt >= (N*N))
+				break;
+			int safe = 0;
+			for(int x =0 ; x < N ; x++) {
+				for(int y = 0; y < N ; y++) {
+					if(rainarea[x][y] == false) {
+						rainarea[x][y] = true;
+						safe++;
+						q.add(new int[]{x,y});
+						while(!q.isEmpty()) {
+							int location[] = q.poll();
+							for(int d = 0 ; d <= 3; d++) {
+								int nx = location[0]+dx[d];
+								int ny = location[1]+dy[d];
+								if(nx >= 0 && ny >= 0 && nx <N && ny < N && rainarea[nx][ny] == false) {
+									q.add(new int[] {nx,ny});
+									rainarea[nx][ny] = true;
+								}
+							}
+
+						}
+					}
+				}
+			}
+			max = Math.max(max, safe);
+			i++;
+		}
+	}
+
+}
---
+++ Java/BOJ_9205.java
@@ -0,0 +1,89 @@
+package algo_0906;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_9205 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 1 ; i <= T ; i++) {
+			int Gnum = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			int sx = Integer.parseInt(st.nextToken());
+			int sy = Integer.parseInt(st.nextToken());
+			Start start= new Start(sx , sy);
+			
+			G c[] = new G[Gnum+1];
+			for(int j = 0 ; j< Gnum ; j++) {
+				st = new StringTokenizer(br.readLine());
+				int cx = Integer.parseInt(st.nextToken());
+				int cy = Integer.parseInt(st.nextToken());
+				c[j] = new G(cx, cy);
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			int fx = Integer.parseInt(st.nextToken());
+			int fy = Integer.parseInt(st.nextToken());
+			c[Gnum] = new G(fx, fy);
+			boolean visited[] = new boolean[Gnum+1];
+			boolean flag = false;
+			
+			Queue<int[]> q = new LinkedList<>();
+			q.add(new int[]{start.x, start.y});
+			while(!q.isEmpty()) {
+				int location[] = q.poll();
+				if( (Math.abs(location[0] - c[Gnum].x) + Math.abs(location[1] - c[Gnum].y)) <= 1000) {
+					flag = true;
+					break;
+				}
+				for(int a = 0 ; a <= Gnum ; a++) {	
+					if(visited[a] == false && (Math.abs(location[0] - c[a].x) + Math.abs(location[1] - c[a].y)) <= 1000) {
+						q.add(new int[] {c[a].x, c[a].y});
+						visited[a] = true;
+					}
+				}				
+			}
+			if(flag == true) {
+				sb.append("happy");
+			}else {
+				sb.append("sad");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
+
+class G{
+	int x;
+	int y;
+	public G(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "GS [x=" + x + ", y=" + y + "]";
+	}
+}
+
+class Start{
+	int x;
+	int y;
+	
+	public Start(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+}
