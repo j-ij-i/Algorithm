7f3550ab7b53ebb8726347b67840e8777ee9eb32
j-ij-i <sunmlee126@gmail.com>
2021-09-23 23:33:45 +0900

    210923

--- Java/BOJ_20056.java
+++ Java/BOJ_20056.java
@@ -5,13 +5,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-class fireball{
+class fireball {
 	int r;
 	int c;
 	int m;
 	int s;
 	int d;
-	
+
 	public fireball(int r, int c, int m, int s, int d) {
 		super();
 		this.r = r;
@@ -20,7 +20,7 @@ class fireball{
 		this.s = s;
 		this.d = d;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "fireball [r=" + r + ", c=" + c + ", m=" + m + ", s=" + s + ", d=" + d + "]";
@@ -28,84 +28,94 @@ class fireball{
 }
 
 public class BOJ_20056 {
-	
-	static int N,M,K;
-	static int dx[] = {-1,-1,0,1,1,1,0,-1};
-	static int dy[] = {0,1,1,1,0,-1,-1,-1};
+
+	static int N, M, K;
+	static int dx[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 	static ArrayList<fireball> map[][];
 	static ArrayList<fireball> fball = new ArrayList<>();
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		
+
 		map = new ArrayList[N][N];
-			
-		//fball ArrayList에 파이어볼정보 하나씩 대입
-		for(int i =0 ; i <M ; i++) {
+		// map을 초기화 시켜주지 않으면 NullPointerException이 발생한다.
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] = new ArrayList<fireball>();
+			}
+		}
+
+		// fball ArrayList에 파이어볼정보 하나씩 대입
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int ri = Integer.parseInt(st.nextToken());
-			int ci = Integer.parseInt(st.nextToken());
+			int ri = Integer.parseInt(st.nextToken())- 1; // 1을 기준으로 넣어주기 때문에 -1인 상태로 넣어줘야함
+			int ci = Integer.parseInt(st.nextToken())- 1;
 			int mi = Integer.parseInt(st.nextToken());
 			int si = Integer.parseInt(st.nextToken());
 			int di = Integer.parseInt(st.nextToken());
-			fball.add(new fireball(ri,ci,mi,si,di));
+			fball.add(new fireball(ri, ci, mi, si, di));
 		}
-		
-		//K번만큼 이동을 명령시킴
-		for(int i = 0; i< K ; i++) {
-			//각 파이어볼 이동
-			for(int j = 0 ; j < fball.size(); j++) {
+
+		// K번만큼 이동을 명령시킴
+		for (int i = 0; i < K; i++) {
+			// 각 파이어볼 이동
+			for (int j = 0; j < fball.size(); j++) {
+//				System.out.println(j);
 				fireball f = fball.get(j);
-				f.r = (f.r + (dx[f.d] * (f.s % N)))%N; // 속력 s가 N을 넘게될 수 있으므로 N%를 계산한 후 
-				f.c = (f.c + (dy[f.d] * (f.s % N)))%N;
-				map[f.c][f.c].add(f);
+				f.r = (f.r+ N+(dx[f.d] * (f.s % N))) % N; // 속력 s가 N을 넘게될 수 있으므로 N%를 계산
+				f.c = (f.c+ N+(dy[f.d] * (f.s % N))) % N; // N을 더하는 이유는 파이어볼이 4개로 나누어질때 범위 밖으로 넘어갈 수 있으므로 N을 더해줘서 안전범위로?
+//				System.out.println(f.c);
+//				System.out.println(f.r);
+				map[f.r][f.c].add(f);
 			}
 			// 파이어볼 판단
-			for(int x = 0 ; x < N ; x++) {
-				for(int y = 0 ; y < N ; y++) {
-					if(map[x][y].size() == 1) {
-						
-					}else if(map[x][y].size() >= 2) {
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < N; y++) {
+					if (map[x][y].size() == 1) {
+						map[x][y].clear();
+					} else if (map[x][y].size() >= 2) {
 						int nm = 0;
 						int ns = 0;
 						int dflag = (map[x][y].get(0).d % 2);
 						boolean flag = true;
-						for(fireball fb : map[x][y]) {
+						for (fireball fb : map[x][y]) {
 							nm += fb.m;
 							ns += fb.s;
-							if((fb.d % 2) != dflag) {
+							if ((fb.d % 2) != dflag) {
 								flag = false;
 							}
-							fball.remove(fb); //몰랐던 부분!! 이런식으로 지울수있구나..
+							fball.remove(fb); // 몰랐던 부분!! 이런식으로 지울수있구나..
 						}
-						
-						if((nm / 5) > 0) {
+						int size = map[x][y].size();
+						map[x][y].clear();
+						if ((nm / 5) > 0) {
 							int a = 0;
-							if(flag == true)
+							if (flag == true)
 								a = 0;
 							else
 								a = 1;
-							
-							for(int t = a ; t < 8;) {
-								fball.add(new fireball(x+dx[t],y+dy[t],1,nm/5, ns/map[x][y].size()));
+							for (int t = a; t < 8;) {
+								fball.add(new fireball(x, y, nm/5, ns/size, t));
+//								System.out.println(flag == true ? "true" : "false");
 								t += 2;
 							}
-							map[x][y].clear();
 						}
-						
+
 					}
 				}
 			}
 		}
-		
-		int result = 0 ;
-		for(fireball f : fball) {
+
+		int result = 0;
+		for (fireball f : fball) {
 			result += f.m;
 		}
 		System.out.println(result);
---
+++ Java/JOL_1681_해밀턴순환회로_fail.java
@@ -0,0 +1,67 @@
+package algo_0923;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class JOL_1681_해밀턴순환회로 {
+	static int map[][];
+	static int N;
+	static int[] input, selected;
+	static boolean[] flag;
+	static int min;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 1 2
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st ;
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+	      for(int x = 0 ; x < N ; x++) {
+	          st = new StringTokenizer(br.readLine());
+	          for(int y = 0; y < N ; y++) {
+	             map[x][y] = Integer.parseInt(st.nextToken());
+	          }
+	       }
+		min = Integer.MAX_VALUE;
+		flag = new boolean[N];
+		selected = new int[N-1];
+		permu(0);
+		
+		if(min == Integer.MAX_VALUE) {
+			System.out.println(0);
+		}else System.out.println(min);
+	}
+	private static void permu(int cnt) {
+		if(cnt == N-1) {
+			int count = 0;
+			if(map[0][selected[0]] == 0) {
+				return;
+			}
+			count += map[0][selected[0]];
+			for(int i = 0; i < N-2; i++) {
+				if(map[selected[i]][selected[i+1]] == 0) {
+					return;
+				}
+				count += map[selected[i]][selected[i+1]]; 
+			}
+			if(map[selected[N-2]][0] == 0) {
+				return;
+			}
+			count += map[selected[N-2]][0];
+			min = Math.min(min, count);
+			return;
+		}
+		
+		for(int i=1; i<N; ++i) {
+			if(!flag[i]){ //사용 안했음		
+				flag[i] = true;
+				selected[cnt] = i; //cnt 위치에 i값 저장
+				permu(cnt+1);//다음 위치에 넣을 값 뽑으러 보냄				
+				flag[i] = false;
+			}
+		}
+	}
+}
