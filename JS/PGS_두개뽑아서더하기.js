261c767b005bd1d2e74fd02186580e7e30623fc5
j-ij-i <sunmlee126@gmail.com>
2021-11-16 23:59:20 +0900

    211116

--- JS/PGS_두개뽑아서더하기.js
+++ JS/PGS_두개뽑아서더하기.js
@@ -1,14 +1,16 @@
 function solution(numbers) {
-    var answer = [];
-    
-    for(let i = 0 ; i < numbers.length; i++){
-        for(let j = i+1 ; j < numbers.length; j++){
-            let sum = numbers[i] + numbers[j];
-            if(answer.indexOf(sum) == -1){
-                answer.push(sum);
-            }
-        }
+  var answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (answer.indexOf(sum) === "") {
+        answer.push(sum);
+      }
     }
-    answer.sort((a,b) => a-b);
-    return answer;
+  }
+  answer.sort((a, b) => a - b);
+  return answer;
 }
+
+// if(answer.find(item => item === sum ) != undefined) continue; 도 가능!
--- JS/PGS_약수의개수와덧셈.js
+++ JS/PGS_약수의개수와덧셈.js
@@ -8,11 +8,9 @@ function solution(left, right) {
         cnt++;
       }
     }
-    if (cnt % 2 === 0) {
-      answer += i;
-    } else {
-      answer -= i;
-    }
+    if (cnt % 2 === 0) answer += i;
+    else answer -= i;
   }
   return answer;
 }
+// 제곱근이 정수일 때 n의 제곱이 되니까 Math.sqrt
