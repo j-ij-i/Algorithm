29abeaf061d8d5d3593a74b28a4b976201287e13
LeeSunmin <sunmlee7@naver.com>
2022-01-17 23:00:16 +0900

    220117

--- Java/BOJ_2293.java
+++ Java/BOJ_2293.java
@@ -1,21 +1,29 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_2293 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); // 3
-		int k = Integer.parseInt(st.nextToken()); // 10
-		int arr[] = new int[n];
-		for(int i = 0 ; i < n ; i++) {
-			arr[i] = Integer.parseInt(br.readLine()); // 1, 2, 5 
-		}
-		
-	}
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int n, k;
+    private static int[] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+        dp = new int[k + 1];
+        dp[0] = 1;
+
+        for(int i = 1 ; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            for (int j = arr[i]; j <= k; j++)
+                dp[j] += dp[j - arr[i]];
+        }
+
+        System.out.println(dp[k]);
+    }
+
 }
