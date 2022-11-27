62215eeb24f73da34bc656a0e73371f6b0ae3d6b
j-ij-i <sunmlee126@gmail.com>
2022-11-27 23:43:40 +0900

    update : 1 problem solved again

--- Python/PGS_롤케이크자르기.py
+++ Python/PGS_롤케이크자르기.py
@@ -8,44 +8,49 @@ def solution(topping):
     # 아래로 내려가는
     # left
     for i in range(0,target):
-        ldic[i] += 1
+        ldic[topping[i]] += 1
         
     # right 
     for i in range(target, len(topping)):
-        rdic[i] += 1
+        rdic[topping[i]] += 1
     
     if len(ldic) == len(rdic):
         answer += 1
-        for t in range(target,0,-1):
-            rdic[t] += 1
-            ldic[t] -= 1
-            if ldic[t] == 0:
-                del rdic[t]
-            if not len(ldic) == len(rdic):
-                break
-            else:
-                answer += 1
+        #print('aa')
     
-    ldic = defaultdict(int)
-    rdic = defaultdict(int)
+    for t in range(target-1,0,-1):
+        rdic[topping[t]] += 1
+        ldic[topping[t]] -= 1
+        if ldic[topping[t]] <= 0:
+            del ldic[topping[t]]
+        if len(ldic) < len(rdic):
+            break
+        elif len(ldic) == len(rdic):
+            answer += 1
+            #print('t',t)
     
     # 위로 올라가는
+    ldic = defaultdict(int)
+    rdic = defaultdict(int)
+
     # left
     for i in range(0,target):
-        ldic[i] += 1
-        
-    # right 
+        ldic[topping[i]] += 1
+
+    # right
     for i in range(target, len(topping)):
-        rdic[i] += 1  
-    
-    for t in range(target,len(topping)-1):
-        rdic[t] -= 1
-        ldic[t] += 1
-        if rdic[t] == 0:
-            del rdic[t]
-        if not len(ldic) == len(rdic):
+        rdic[topping[i]] += 1
+
+    for t in range(target,len(topping)):
+        rdic[topping[t]] -= 1
+        ldic[topping[t]] += 1
+        if rdic[topping[t]] <= 0:
+            del rdic[topping[t]]
+        if len(ldic) > len(rdic):
             break
-        else:
+        elif len(ldic) == len(rdic):
             answer += 1
-    
+            #print('z',t)
+            
+
     return answer
