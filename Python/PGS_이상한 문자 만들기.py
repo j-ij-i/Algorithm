8cb677c59ff7b3072a3d76e5e1e8147703e73174
j-ij-i <sunmlee126@gmail.com>
2022-12-23 23:51:07 +0900

    [221223] update : PGS 이상한 문자 resolved (python)

--- Python/PGS_이상한 문자 만들기.py
+++ Python/PGS_이상한 문자 만들기.py
@@ -3,18 +3,13 @@ def solution(s):
     flag = False
     cnt = 0
     for i in range(len(s)):
-        if not flag and s[i] == 0:
-            answer += s[i]
-        elif not flag and not s[i] == 0:
-            flag = True
+        if s[i] == ' ':
             cnt = 0
-            print(s[i])
-            answer += (s[i].upper())
-            cnt += 1
-        elif flag:
+            answer += s[i]
+        elif not s[i] == ' ':
             if cnt % 2 == 0:
-                answer += (s[i].upper())
+                answer += s[i].upper()
             else:
-                answer += (s[i].lower())
+                answer += s[i].lower()
             cnt += 1
     return answer
