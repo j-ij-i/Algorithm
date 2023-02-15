d5d7dc5b0339fed8fca0a96a48b3cf337932b9bc
j-ij-i <sunmlee126@gmail.com>
2023-02-15 23:53:20 +0900

    [230215] update : PGS 옷가게 할인받기 solved (python)

---
+++ Python/PGS_옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    answer = 0
+    if price < 100000:
+        return price
+    if price >= 500000:
+        return int(price*(0.8))
+    if price >= 300000:
+        return int(price*(0.9))
+    if price >= 100000:
+        return int(price*(0.95))
